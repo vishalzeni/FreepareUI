b2a69fd773a82a000fa35248dafaa01c0b2da935
vishalzeni <vishalzenith47@gmail.com>
2025-03-10 16:02:40 +0530

    removed service

--- src/index.tsx
+++ src/index.tsx
@@ -18,14 +18,7 @@ const Main = () => {
     </ThemeProvider>
   );
 };
-// Service Worker Registration
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((reg) => console.log('Service Worker registered:', reg))
-      .catch((err) => console.log('Service Worker registration failed:', err));
-  });
-}
+
 root.render(
   <React.StrictMode>
     <Main />
